5f79f181a83c22ef61ee77b91c493a80731d035a
Max Blackwell <maxmac001@gmail.com>
2017-07-12 15:59:35 -0700

    test summary

---
+++ gitTest2/gitTest2/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  gitTest2
+//
+//  Created by Max Blackwell on 7/12/17.
+//  Copyright © 2017 Max Blackwell. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+
+    
+    override func didMove(to view: SKView) {
+        
+        print("changes were made")
+    }
+    
+    
+}
